c5f29a7d15b489806e4b56c3406753a475f7ef8a
Mayank <mayank283@live.com>
2017-07-16 02:14:36 +0530

    Making env-specific changes properties

--- src/main/java/org/mayank/restapp/simple/security/JwtAuthenticationFilter.java
+++ src/main/java/org/mayank/restapp/simple/security/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +17,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import io.jsonwebtoken.JwtException;
 
 public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+	// ~ Instance fields
+	// ================================================================================================
+
+	@Value("${access.origin}")
+	private String origin;
+
+	@Value("${access.headers}")
+	private String headers;
+
+	@Value("${access.methods}")
+	private String methods;
+
+	@Value("${access.age}")
+	private String age;
+
+	// ~ Methods
+	// ========================================================================================================
 
 	public JwtAuthenticationFilter(String defaultFilterProcessesUrl) {
 		super(defaultFilterProcessesUrl);
@@ -31,15 +50,15 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException, IOException, ServletException {
-
+		
 		System.out.println(request.getMethod());
 		if (request.getMethod().equals(RequestMethod.GET.toString())) {
 
-			response.setHeader("Access-Control-Allow-Origin", "http://ec2-13-126-239-177.ap-south-1.compute.amazonaws.com");
-			response.setHeader("Access-Control-Allow-Headers", "authorization");
-			response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE");
-			response.setHeader("Access-Control-Max-Age", "1800");
-			
+			response.setHeader("Access-Control-Allow-Origin", origin);
+			response.setHeader("Access-Control-Allow-Headers", headers);
+			response.setHeader("Access-Control-Allow-Methods", methods);
+			response.setHeader("Access-Control-Max-Age", age);
+
 			String header = request.getHeader("Authorization");
 			if (header.isEmpty() || header == null) {
 				throw new JwtException("No JWT token found in request headers");
@@ -47,13 +66,12 @@ public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFil
 
 			JwtAuthenticationToken authRequest = new JwtAuthenticationToken(header);
 			return getAuthenticationManager().authenticate(authRequest);
-		}
-		else{
-			response.setHeader("Access-Control-Allow-Origin", "http://ec2-13-126-239-177.ap-south-1.compute.amazonaws.com");
-			response.setHeader("Access-Control-Allow-Headers", "authorization");
-			response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE");
-			response.setHeader("Access-Control-Max-Age", "1800");
-			return null;	
+		} else {
+			response.setHeader("Access-Control-Allow-Origin", origin);
+			response.setHeader("Access-Control-Allow-Headers", headers);
+			response.setHeader("Access-Control-Allow-Methods", methods);
+			response.setHeader("Access-Control-Max-Age", age);
+			return null;
 		}
 	}
 
